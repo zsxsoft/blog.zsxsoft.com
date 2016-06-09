71b73f529ed70a9d2f09e622e991a8c1056c3d7d
zsx <zsx@zsxsoft.com>
2016-06-09 23:33:48 +0800

    Add CSS

---
+++ src/app/Components/Container.jsx
@@ -0,0 +1,8 @@
+///<reference path="../../../typings/tsd.d.ts" />
+import React from 'react';
+
+export default class Container extends React.Component {
+    render() {
+        return <div className="container">{this.props.children}</div>;
+    }
+}

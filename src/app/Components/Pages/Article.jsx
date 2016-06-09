ac9d2e5c6343588853a6df9704eb68b047de0725
zsx <zsx@zsxsoft.com>
2016-06-10 00:46:46 +0800

    Most of them finished

--- src/app/Components/Pages/Article.jsx
+++ src/app/Components/Pages/Article.jsx
@@ -4,14 +4,14 @@ import ReactDOM from 'react-dom';
 import {Link, Router} from 'react-router';
 import Container from '../Container';
 import Config from '../../config';
-import {formatDate, formatArticleContent, isMobile as checkMobile} from '../../utils';
-import {default as EmbedDuoshuo} from '../Duoshuo/EmbedThread';
-import {default as ShareDuoshuo} from '../Duoshuo/Share';
-import {default as ExtensionDuoshuo} from '../Duoshuo/Extensions';
+import {formatDate, formatArticleContent, filterHtml, isMobile as checkMobile} from '../../utils';
+import EmbedDuoshuo from '../Duoshuo/EmbedThread';
+import ShareDuoshuo from '../Duoshuo/Share';
+import ExtensionDuoshuo from '../Duoshuo/Extensions';
+import {Navbar, Nav, NavItem, Button, Col, Row, Well, Image, ListGroup, ListGroupItem} from 'react-bootstrap';
+import Ink from 'react-ink';
 
 let isMobile = checkMobile();
-let articleResizeId = 0;
-let resizeChangeWidth;
 
 export default class Article extends React.Component {
 
@@ -19,9 +19,6 @@ export default class Article extends React.Component {
     constructor(props) {
         super(props);
         let self = this;
-        resizeChangeWidth = (focus) => {
-            resizeChangeWidthOriginal(self, focus);
-        }
         fetch(Config.url + this.props.location.pathname).then((data) => {
             return data.json();
         }).then((json) => {
@@ -35,13 +32,10 @@ export default class Article extends React.Component {
     }
 
     componentDidMount() {
-        resizeChangeWidth(true);
-        articleResizeId = window.resizeQueue.add(resizeChangeWidth);
     }
 
     componentWillUnmount() {
         this.setState({ mounted: false });
-        window.resizeQueue.remove(articleResizeId);
     }
 
     render() {
@@ -52,7 +46,30 @@ export default class Article extends React.Component {
             let contentHtml = { __html: formatArticleContent(article.Content) };
             let linkTo = "/post/" + article.ID;
             document.title = article.Title + " - " + Config.title;
-            
+            childContext = <div>
+                <Container>
+
+                    <Well bsSize="large" key={article.ID}>
+                        <Button style={{ width: "100%", fontSize: 20 }}><Link to={linkTo}>{article.Title}<Ink/></Link></Button>
+                        <Row>
+                            <Col md={2} xs={5}>
+                                <Image circle src={article.Author.Avatar} style={{ verticalAlign: "middle", marginRight: 5, height: 32 }}/>{article.Author.StaticName}
+                            </Col>
+                            <Col style={{ textAlign: "right" }}>
+                                <span style={{ color: "rgba(0, 0, 0, 0.54)", fontSize: isMobile ? 10 : 14, marginTop: 7 }}>
+                                    {formatDate(article.PostTime) } in {article.Category.Name}
+                                    <span> / </span><ExtensionDuoshuo type="thread-count" duoshuoKey={article.ID} title={article.Title} url={article.Url} content={filterHtml(article.Intro) } />
+                                    <span> / </span>{article.ViewNums}
+                                </span>
+                            </Col>
+                        </Row>
+                        <Row><br/></Row>
+                        <Row><article dangerouslySetInnerHTML={contentHtml}></article></Row>
+                        <Row><Container><Col mdOffset={1}><ShareDuoshuo type="share" duoshuoKey={article.ID} title={article.Title} url={article.Url} content={article.Intro} /></Col></Container></Row>
+                        <Row><EmbedDuoshuo duoshuoKey={article.ID} title={article.Title} url={article.Url} /></Row>
+                    </Well>
+                </Container>
+            </div>
         }
         return (
             <Container>
@@ -66,26 +83,6 @@ export default class Article extends React.Component {
 
 /*
 childContext = (
-                <div style={{ paddingTop: isMobile ? 60 : 15, position: "relative", float: "left" }}>
-                    <AutoResponsive ref="container" {...this.getAutoResponsiveProps() }>
-                        <div style={singleTargetStyle} key={article.ID}>
-                            <Card>
-                                <CardText>
-                                    <Link to={linkTo} style={{ textDecoration: "none !important" }}><FlatButton style={{ width: "100%" }}><CardTitle title={article.Title} style={{ position: "inherit" }}/></FlatButton></Link>
-                                    <div style={{ marginBottom: 10 }}>
-                                        <Avatar src={article.Author.Avatar} style={{ verticalAlign: "middle", marginRight: 5 }}/>zsx
-                                        <span style={{ color: "rgba(0, 0, 0, 0.54)", float: "right", marginTop: 7, fontSize: isMobile ? 10 : 14 }}>
-                                            {formatDate(article.PostTime) } in {article.Category.Name}
-                                            <span> / </span><ExtensionDuoshuo type="thread-count" duoshuoKey={article.ID} title={article.Title} url={article.Url} content={article.Intro} />
-                                            <span> / </span>{article.ViewNums}
-                                        </span>
-                                    </div>
-                                    <article dangerouslySetInnerHTML={contentHtml}></article>
-                                    <ShareDuoshuo type="share" duoshuoKey={article.ID} title={article.Title} url={article.Url} content={article.Intro} />
-                                    <EmbedDuoshuo duoshuoKey={article.ID} title={article.Title} url={article.Url} />
-                                </CardText>
-                            </Card>
-                        </div>
-                    </AutoResponsive></div>
+                
             );
  */
--- src/app/Components/Pages/List.jsx
+++ src/app/Components/Pages/List.jsx
@@ -1,14 +1,15 @@
 ///<reference path="../../../../typings/tsd.d.ts" />
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Link, Router, PropTypes} from 'react-router';
+import {Link, Router} from 'react-router';
+import {formatDate, isMobile as checkMobile, filterHtml, getNewListUri, jsonConcat} from '../../utils';
+import ExtensionDuoshuo from '../Duoshuo/Extensions';
+import {Navbar, Nav, NavItem, Button, Col, Well, Image, ListGroup, ListGroupItem} from 'react-bootstrap';
 import Container from '../Container';
 import Config from '../../config';
-import {formatDate, isMobile as checkMobile, filterHtml, getNewListUri, jsonConcat} from '../../utils';
-import {default as ExtensionDuoshuo} from '../Duoshuo/Extensions';
+import Ink from 'react-ink';
 
 let isMobile = checkMobile();
-let listResizeId = 0;
 export default class List extends React.Component {
 
     initState(props) {
@@ -18,7 +19,7 @@ export default class List extends React.Component {
             this.setState({ data: json });
         });
     }
-    
+
     constructor(props) {
         super(props);
         this.initState(props);
@@ -57,104 +58,95 @@ export default class List extends React.Component {
         this.context.history.pushState(null, url);
     }
 
+    getSidebarContainer(sidebar) {
+        let that = this;
+        let sidebarContainer = <div/>;
+        let contentHtml = { __html: sidebar.Content };
+
+        if (sidebar.Type === "div") {
+            if (sidebar.HtmlID === "Duoshuo_RecentComments") {
+                sidebarContainer = (<ExtensionDuoshuo type="recent-comments" data-num-items="5"/>);
+            } else {
+                sidebarContainer = (<div id={sidebar.HtmlID} dangerouslySetInnerHTML={contentHtml}></div>);
+            }
+            return sidebarContainer;
+        }
+
+        let result;
+        let reg = /<li(.*?)>([\w\W]*?)<\/li>/gi;
+        let liContainer = [];
+        while ((result = reg.exec(sidebar.Content)) !== null) {
+            let listObject = (htmlContent => {
+                let ret = {
+                    html: null,
+                    attr: {},
+                };
+                let result;
+                let reg = /<a(.*?)href="(.*?)"(.*?)>([\w\W]*?)<\/a>/gi;
+                if ((result = reg.exec(htmlContent)) !== null) {
+                    ret.html = result[4];
+                    ret.attr.onClick = that.sidebarListClick.bind(that, result[2]);
+                } else {
+                    ret.html = <div dangerouslySetInnerHTML={{ __html: htmlContent }} />;
+                }
+                return ret;
+            })(result[2]);
+            console.log(listObject);
+            liContainer.push(<ListGroupItem key={result.index} {...listObject.attr}><Button>{listObject.html}</Button><Ink/></ListGroupItem>);
+        }
+        sidebarContainer = (<ListGroup id={sidebar.HtmlID}>{liContainer}</ListGroup>);
+        return sidebarContainer;
+    }
+
     render() {
         let that = this;
         let data = this.state.data;
-        return (<div></div>);
-        /*
-            <FullWidthSection>
-                {childContext}
+        let childContext = (<div>Please wait..</div>);
+        if (data) {
+            childContext = (<div>
+                <Col md={8} xs={12}>
+                    {data.articles.map(article => {
+                        let introHtml = { __html: article.Intro };
+                        let linkTo = "/post/" + article.ID;
+                        return (<Well bsSize="large" key={article.ID}>
+                            <Button style={{ width: "100%", fontSize: 20 }}><Link to={linkTo}>{article.Title}<Ink/></Link></Button>
+                            <div><div dangerouslySetInnerHTML={introHtml}></div></div>
+                            <div>
+                                <Col md={2} xs={5}>
+                                    <Image circle src={article.Author.Avatar} style={{ verticalAlign: "middle", marginRight: 5, height: 32 }}/>{article.Author.StaticName}
+                                </Col>
+                                <Col style={{ textAlign: "right" }}>
+                                    <span style={{ color: "rgba(0, 0, 0, 0.54)", fontSize: isMobile ? 10 : 14, marginTop: 7 }}>
+                                        {formatDate(article.PostTime) } in <Link to={getNewListUri(this.props.location.pathname, { cate: article.Category.ID }) }>{article.Category.Name}</Link>
+                                        <span> / </span><ExtensionDuoshuo type="thread-count" duoshuoKey={article.ID} title={article.Title} url={article.Url} content={filterHtml(article.Intro) } />
+                                        <span> / </span>{article.ViewNums}
+                                    </span>
+                                </Col>
+                            </div>
+                        </Well>);
+                    }) }
+                </Col>
+                <Col md={4} xs={12}>
+                    {data.sidebar.map(sidebar => {
+                        return <Well bsSize="large" key={sidebar.HtmlID}>
+                        <p style={{ width: "100%", fontSize: 20 }}>{sidebar.Name}</p>
+                        {this.getSidebarContainer(sidebar)}
+                        </Well>
+                    }) }
+                </Col>
 
-            </FullWidthSection>
-        */
+                <div style={{ position: "fixed", top: "90%", right: "2%", width: "120px", zIndex: 100000 }}>
+                    <Link to={this.pageTo(-1) }><Button bsStyle="primary btn-fab" style={{ float: "left" }}>
+                        <span style={{ color: "white" }}>&lt; </span><Ink/>
+                    </Button></Link>
+                    <Link to={this.pageTo(1) }><Button bsStyle="primary btn-fab" style={{ float: "right" }}>
+                        <span style={{ color: "white" }}>&gt; </span><Ink/>
+                    </Button></Link>
+                </div>
+            </div>);
+        }
+        return (<Container>{childContext}</Container>);
     }
 
 };
-List.contextTypes = { history: PropTypes.history }
-
-
-
-/*
-<div style={{ paddingTop: isMobile ? 50 : 15 }} className="list-container">
-                <AutoResponsive ref="container" {...this.getAutoResponsiveProps() }>
-                    {data.articles.map((article) => {
-                        let introHtml = { __html: article.Intro };
-                        let linkTo = "/post/" + article.ID;
-                        return (<Card style={singleTargetStyle} key={article.ID}>
-                            <Link to={linkTo}><FlatButton style={{ width: "100%" }}><CardTitle title={article.Title} style={{ position: "inherit", padding: 10, height: 30, overflow: "hidden" }}/></FlatButton></Link>
-                            <CardText dangerouslySetInnerHTML={introHtml} style={cardTextStyle}>
-                            </CardText>
-                            <CardText>
-                                <Avatar src={article.Author.Avatar} style={{ verticalAlign: "middle", marginRight: 5 }}/>zsx
-                                <span style={{ color: "rgba(0, 0, 0, 0.54)", float: "right", marginTop: 7, fontSize: isMobile ? 10 : 14 }}>
-                                    {formatDate(article.PostTime) } in <Link to={getNewListUri(this.props.location.pathname, { cate: article.Category.ID }) }>{article.Category.Name}</Link>
-                                    <span> / </span><ExtensionDuoshuo type="thread-count" duoshuoKey={article.ID} title={article.Title} url={article.Url} content={filterHtml(article.Intro) } />
-                                    <span> / </span>{article.ViewNums}
-                                </span>
-                            </CardText>
-                        </Card>);
-                    }) }
-                    {data.sidebar.map((sidebar) => {
-                        let sidebarStyle = this.state.responsiveStyle;
-                        let sidebarSingleState = this.state["sidebar_" + sidebar.HtmlID];
-                        sidebarStyle.position = "relative";
-                        if (sidebarSingleState) {
-                            sidebarStyle = jsonConcat(sidebarStyle, this.state["sidebar_" + sidebar.HtmlID]);
-                        }
-                        let contentHtml = { __html: sidebar.Content };
-                        let sidebarContainer;
-
-                        if (sidebar.Type === "div") {
-                            // For duoshuo
-                            if (sidebar.HtmlID === "Duoshuo_RecentComments") {
-                                sidebarContainer = (<ExtensionDuoshuo type="recent-comments" data-num-items="5"/>);
-                            } else {
-                                sidebarContainer = (<div id={sidebar.HtmlID} dangerouslySetInnerHTML={contentHtml}></div>);
-                            }
-
-                        } else {
-                            let result;
-                            let reg = /<li(.*?)>([\w\W]*?)<\/li>/gi;
-                            let liContainer = [];
-                            while ((result = reg.exec(sidebar.Content)) !== null) {
-                                let listObject = ((htmlContent) => {
-                                    let ret = {
-                                        html: null,
-                                        attr: {},
-                                    };
-                                    let result;
-                                    let reg = /<a(.*?)href="(.*?)"(.*?)>([\w\W]*?)<\/a>/gi;
-                                    if ((result = reg.exec(htmlContent)) !== null) {
-                                        ret.attr.primaryText = result[4];
-                                        ret.attr.onClick = that.sidebarListClick.bind(that, result[2]);
-                                    } else {
-                                        ret.html = <div dangerouslySetInnerHTML={{ __html: htmlContent }} />;
-                                    }
-                                    return ret;
-                                })(result[2]);
-                                liContainer.push(<ListItem key={result.index} {...listObject.attr}>{listObject.html}</ListItem>);
-                            }
-                            sidebarContainer = (<List id={sidebar.HtmlID}>{liContainer}</List>);
-                        }
-                        return (
-                            <Card style={sidebarStyle} key={sidebar.HtmlID}>
-                                <CardTitle title={sidebar.Name}/>
-                                <FlatButton onClick={that.resizeCard.bind(that, "sidebar_" + sidebar.HtmlID) } backgroundColor="#000000" style={{ fontSize: 32, color: "#ffffff", height: 32, position: "absolute", bottom: 0, width: "100%", opacity: 0.7, zIndex: 100000, float: "left", verticalAlign: "bottom" }}>
-                                    <i className="material-icons">{sidebarSingleState ? sidebarSingleState.keyboardArrow : "keyboard_arrow_down"}</i>
-                                </FlatButton>
-                                <CardText>
-                                    {sidebarContainer}
-                                </CardText>
-                            </Card>)
-                    }) }
-                </AutoResponsive>
-                <div style={{ position: "fixed", top: "90%", right: "2%", width: "120px", zIndex: 100000 }}>
-                    <Link to={this.pageTo(-1) }><FloatingActionButton style={{ float: "left" }}>
-                        <span style={{ color: "white" }}>&lt; </span>
-                    </FloatingActionButton></Link>
-                    <Link to={this.pageTo(1) }><FloatingActionButton style={{ float: "right" }}>
-                        <span style={{ color: "white" }}>&gt; </span>
-                    </FloatingActionButton></Link>
-                </div>
-            </div>
- */
+//List.contextTypes = { history: PropTypes.history }

63a8b7c7dadb859b569095961486a94039ff5ea8
zsx <zsx@zsxsoft.com>
2016-06-09 20:27:26 +0800

    Update player to fix it code style

--- src/www/player.js
+++ src/www/player.js
@@ -1,13 +1,13 @@
 (function () {
-	var isIframeLoaded = false;
-	var playerDOM = document.getElementById("player");
-	var cloudMusic = "http://music.163.com/outchain/player?type=0&id=361168546&auto=1"; 
+	let isIframeLoaded = false;
+	let playerDOM = document.getElementById("player");
+	let cloudMusic = "http://music.163.com/outchain/player?type=0&id=361168546&auto=1"; 
 	if (/musicPlay=1/.test(document.cookie)) {
 		loadIframe(cloudMusic);
 	} 
 	
 	document.querySelector(".switch-angle").addEventListener("click", function () {
-		if (playerDOM.className == "") {
+		if (playerDOM.className === "") {
 			playerDOM.className = "show";
 			if (!isIframeLoaded) loadIframe(cloudMusic);
 			document.cookie = "musicPlay=1";

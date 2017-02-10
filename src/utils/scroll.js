59476515e72808bc186ac1f55a0e9e274e50d9ad
zsx <zsx@zsxsoft.com>
2017-02-10 20:20:45 +0800

    Last commit (maybe)

--- src/utils/scroll.js
+++ src/utils/scroll.js
@@ -1,6 +1,14 @@
 import dynamics from 'dynamics.js'
 window.dynamics = dynamics
-export const animateToPosition = (to = 0, element = document.documentElement, time = 1000) => {
+export const animateToPosition = (to = 0, elementArgument = null, time = 1000) => {
+  let element = null
+  if (elementArgument === null) {
+    if (document.body.scrollTop > 0) element = document.body
+    else {
+      /* if (document.documentElement.scrollTop > 0) */
+      element = document.documentElement
+    }
+  }
   dynamics.animate(element, {
     scrollTop: to
   }, {
@@ -8,5 +16,4 @@ export const animateToPosition = (to = 0, element = document.documentElement, ti
     duration: time
   })
 }
-
 export const animateToTop = () => animateToPosition(0)
